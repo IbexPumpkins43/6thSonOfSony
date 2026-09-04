c584b261be7cbf27fc91024506c4b51887ae36ee
IbexPumpkins43 <283789752+IbexPumpkins43@users.noreply.github.com>
2026-09-05 00:31:28 +0100

    Greatly simplify the flake.nix

--- flake.nix
+++ flake.nix
@@ -1,84 +1,23 @@
 {
-  description = "Dukebox - a Discord music bot written in Rust";
+  description = "dukebox";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs = { nixpkgs, ... }:
     let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-      pkgsFor = system: import nixpkgs {
-        inherit system;
-      };
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
     in
     {
-      devShells = forAllSystems (system:
-        let
-          pkgs = pkgsFor system;
-        in
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              cargo
-              rustc
-              rustfmt
-              clippy
-
-              pkg-config
-              opus
-              ffmpeg
-              yt-dlp
-            ];
-
-            shellHook = ''
-              echo "Dukebox development environment"
-              echo "  cargo run --release"
-              echo "  cargo clippy"
-              echo "  cargo fmt"
-            '';
-          };
-        });
-
-      apps = forAllSystems (system:
-        let
-          pkgs = pkgsFor system;
-
-          dukebox-run = pkgs.writeShellApplication {
-            name = "dukebox";
-
-            runtimeInputs = with pkgs; [
-              cargo
-              rustc
-              pkg-config
-              opus
-              ffmpeg
-              yt-dlp
-            ];
-
-            text = ''
-              if [ ! -f Cargo.toml ]; then
-                echo "Run 'nix run' from the Dukebox project directory." >&2
-                exit 1
-              fi
-
-              exec cargo run --release -- "$@"
-            '';
-          };
-        in
-        {
-          default = {
-            type = "app";
-            program = "${dukebox-run}/bin/dukebox";
-          };
-        });
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          cargo
+          rustc
+          pkg-config
+          opus
+          ffmpeg
+          yt-dlp
+        ];
+      };
     };
 }
